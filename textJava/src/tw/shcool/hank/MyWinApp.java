e09b12462aa18143ce17a275303c29319fded719
hank <gorillaz1801058@gmail.com>
2021-12-28 22:06:38 +0800

    2021/12/28

---
+++ textJava/src/tw/shcool/hank/MyWinApp.java
@@ -0,0 +1,15 @@
+package tw.shcool.hank;
+//專案開啟示範流程,這裡主要是將excel多國語系轉換,https://www.youtube.com/watch?v=j1buJD0I0MI,第一part部分,但沒有老師沒有成功之後補上
+//1.創建專案這邊是tw.school.hank
+//2.創建resource
+//2.將excel檔案放進去,等需要用的外部資源放在resource
+//3.這樣就可以去使用外部資源
+//4.那大家想說為什麼我一直可以使用,那是因為你在日蝕裡去使用,打包出去不放resource就會卡關
+public class MyWinApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
